5bc49efe3e455ba74488f11afdd8e2d4a727c72a
sukra <sukra@DESKTOP-OB7AJV3>
2025-02-09 21:02:17 +0300

    Add project files.

---
+++ ConsoleApp22/Araba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    public class Car
+    {
+        public DateTime ProductionDate { get; set; }
+        public string SerialNumber { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int NumberOfDoors { get; set; }
+    }
+}
---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp22;
+
+List<Car> cars = new List<Car>(); //Arabaları saklamak için liste.
+while (true)
+{
+    Console.WriteLine("Araba üretmek istiyor musunuz?(e -h)");
+    string userInput = Console.ReadLine().ToLower();
+    if ("e" == userInput)
+    {
+        //Araba nesnesini üretiyoruz.
+        Car car = new Car();
+        car.ProductionDate = DateTime.Now; //Arabanın üretim tarihi nesne oluşturulduğunda tanımlanıyor.
+        Console.Write("Arabanın markasını girin:");
+        car.Brand = Console.ReadLine();
+        Console.Write("Arabanın modelini girin:");
+        car.Model = Console.ReadLine();
+        Console.Write("Arabanın rengini giriniz:");
+        car.Color = Console.ReadLine();
+        Console.Write("Arabanın seri numarasını giriniz:");
+        car.SerialNumber = Console.ReadLine();
+    CarDoor: Console.WriteLine("Arabanın kapı sayısını giriniz:");
+        try
+        {
+            car.NumberOfDoors = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Lütfen sayı giriniz.");
+            goto CarDoor;
+        }
+        cars.Add(car);
+        Console.WriteLine("Araba başarıyla üretildi.");
+    }
+
+    else if ("h" == userInput)
+    {
+        Console.WriteLine("Üretilen arabalar:");
+        foreach (Car car in cars)
+        {
+            Console.WriteLine($"Arabanın Markası: {car.Brand} Modeli: {car.Model} Seri Numarası: {car.SerialNumber} " +
+                $"Rengi: {car.Color} Kapı Sayısı: {car.NumberOfDoors}");
+        }
+        Console.WriteLine("Programdan çıkılıyor.");
+        break;
+    }
+    else
+        Console.WriteLine("Geçerisiz bir giriş yaptınız.");
+}
